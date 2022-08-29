46e8a7a66387729bd6505b4cd880531a0302253c
Matheus Alencastro <alencastromatheus@gmail.com>
2022-08-29 15:54:54 -0300

    Add project files.

---
+++ WEB.Aula01/Cadastro.cs
@@ -0,0 +1,11 @@
+﻿namespace WEB.Aula01
+{
+    public class Cadastro
+    {
+        public string Cpf { get; set; }
+        public string NamePeople { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Age { get { return DateTime.Now.Year - Birthday.Year; } }
+
+    }
+}
---
+++ WEB.Aula01/Controllers/CadastroController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Aula01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CadastroController : ControllerBase
+    {
+        private static readonly string[] Nomes = new[]
+        {
+        "Matheus", "João", "José", "André", "Maria Luiza", "Louise", "Carlos", "Leonardo", "Antônio", "Ricardo" };
+
+        public List<Cadastro> Cadastros { get; set; } = new List<Cadastro>();
+
+        public CadastroController()
+        {
+            Cadastros = Enumerable.Range(1,6).Select(index => new Cadastro
+            {
+                Cpf = Services.GerarCpf(), //CPFRANDOM
+                NamePeople = Nomes[Random.Shared.Next(Nomes.Length)],
+                Birthday = Services.RandomDay(),
+            }
+            ).ToList();
+        }
+
+        [HttpPost("Adicionar pessoas")]
+        public Cadastro Post(Cadastro informacoes)
+        {
+            Cadastros.Add(informacoes);
+            return informacoes;
+        }
+
+        [HttpGet("Consultar lista")]
+        public List<Cadastro> Get()
+        {
+            return Cadastros;
+        }
+
+        [HttpDelete("Deletar usuário")]
+        public void Delete(string cpf)
+        {
+            var cadastroSelecionado = Cadastros.RemoveAll(x => x.Cpf == cpf);
+        }
+
+        [HttpPut("Atualizar cadastro")]
+        public Cadastro Put(string cpf, Cadastro cadastroAtualizado)
+        {
+            var cadastroSelecionado = Cadastros.FindIndex(x => x.Cpf == cpf);
+            Cadastros[cadastroSelecionado] = cadastroAtualizado;
+            return Cadastros[cadastroSelecionado];
+        }
+    }
+}
